44fdd54659e53cafb150c60bd6bc5a6fa48d295c
ConradoClarke <conradoclarke@gmail.com>
2022-11-06 22:58:44 -0800

    lots of tests

--- Assets/Scripts/RPG/EnemyCharacterTag.cs
+++ Assets/Scripts/RPG/EnemyCharacterTag.cs
@@ -2,6 +2,7 @@ using Licht.Unity.Objects;
 using UnityEngine;
 
 [RequireComponent(typeof(Character))]
+[DefaultExecutionOrder(-1)]
 public class EnemyCharacterTag : SceneObject<EnemyCharacterTag>
 {
     public Character Character { get; private set; }
--- Assets/Scripts/RPG/PlayerCharacterTag.cs
+++ Assets/Scripts/RPG/PlayerCharacterTag.cs
@@ -2,6 +2,7 @@ using Licht.Unity.Objects;
 using UnityEngine;
 
 [RequireComponent(typeof(Character))]
+[DefaultExecutionOrder(-1)]
 public class PlayerCharacterTag : SceneObject<PlayerCharacterTag>
 {
     public Character Character { get; private set; }
